70b99474d58c7c77ccef3e4604f333caeb1f2417
mhnd <daoudmhnd@gmail.com>
2022-07-13 18:43:15 +0300

    rewrote from the start and achieved the desired results for indexing the characters, now the joining of characters into desired password patterns is what left.

--- munchy.py
+++ munchy.py
@@ -13,90 +13,44 @@ to look for a hashed password
 7 need to make sure passwords aren't repeated
 """
 import string
-from random import *
 
-print("""
-Welcome to Munchy, a smart password recovery utility.
-1. To start enter the length or the range of your password
-2. if you remember a pattern you can enter it also
-3. you can choose if the password consist of only letters or digits or special characters
-4. you can choose a name for the output list
-""")
-print("Enter Pattern:")
-lst = list()
+# from random import *
+dic = {}
 
 
-def pattern_loader(userinput):
-    for singleChar in userinput:
-        if singleChar == "A" or singleChar == "a" or singleChar == "1" or singleChar == "*" or singleChar == " ":
-            lst.append(singleChar)
+def main():
+    user_input = input("input:")
+    for index, value in enumerate(user_input):
+        # print(index, value)
+        if value == " ":
+            pass
+        if value == "1":
+            for i in string.digits:
+                # print(index, i)
+                dic[index] = string.digits
+                # print(dic)
+        elif value == "A":
+            for i in string.ascii_uppercase:
+                # print(index, i)
+                dic[index] = string.ascii_uppercase
+        elif value == "a":
+            for i in string.ascii_lowercase:
+                # print(index, i)
+                dic[index] = string.ascii_lowercase
+        elif value == "*":
+            # print(index, string.punctuation)
+            dic[index] = string.punctuation
         else:
-            print("not a valid pattern")
-            break
-    return lst
+            value = None
+            print("None")
+            pass
+
+    return dic
 
 
-pattern_loader(input())
-number_of_passwords = int(input("How many passwords to generate? "))
-for i in range(number_of_passwords):
-    if "A" and "a" and "1" and "*" in lst:
-        password = "".join(choice(string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation) for x in range(len(lst)))
-        print(password)
-    elif "A" and "a" in lst:
-        password = "".join(choice(string.ascii_uppercase + string.ascii_lowercase) for x in range(len(lst)))
-        print(password)
-    elif "A" and "1" in lst:
-        password = "".join(choice(string.ascii_uppercase + string.digits) for x in range(len(lst)))
-        print(password)
-    elif "A" and "*" in lst:
-        password = "".join(choice(string.ascii_uppercase + string.punctuation) for x in range(len(lst)))
-        print(password)
-    elif "1" and "a" in lst:
-        password = "".join(choice(string.digits + string.ascii_lowercase) for x in range(len(lst)))
-        print(password)
-    elif "*" and "a" in lst:
-        password = "".join(choice(string.punctuation + string.ascii_lowercase) for x in range(len(lst)))
-        print(password)
-    elif "1" and "*" in lst:
-        password = "".join(choice(string.digits + string.punctuation) for x in range(len(lst)))
-        print(password)
-    elif "A" in lst:
-        password = "".join(choice(string.ascii_uppercase) for x in range(len(lst)))
-        print(password)
-    elif "a" in lst:
-        password = "".join(choice(string.ascii_lowercase) for x in range(len(lst)))
-        print(password)
-    elif "1" in lst:
-        password = "".join(choice(string.digits) for x in range(len(lst)))
-        print(password)
-    elif "*" in lst:
-        password = "".join(choice(string.punctuation) for x in range(len(lst)))
-        print(password)
-    else:
-        print("invalid pattern")
-# To fight randomness or not ???
-
-
-"""
-  # will be deprecated when a database will be added
-# length = int(input("Enter Length: "))
-pattern = input("Enter Pattern: ")
-for char in pattern:
-    # print(char)
-    if char == "A":
-        # print("A")
-        strings = string.ascii_letters
-        if char == "a":
-            # print("a")
-            strings = string.ascii_letters
-            if char == "1":
-                # print("1")
-                digits = string.digits
-                if char == "*":
-                    # print("*")
-                    specChars = string.punctuation
-                else:
-                    print("error")
-                    pass
-"""
-#
+if __name__ == "__main__":
+    main()
+    # print(dic)
+    for i in dic.values():
+        for x in i:
+            print(x)
